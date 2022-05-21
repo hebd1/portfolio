587b3b764cd08f71dcfe59f53c406ae3d2af5df0
hebd1 <elihebdon@gmail.com>
2022-05-21 01:13:17 -0600

    dynamic navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
+  scrolled!: boolean;
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
+    this.scrolled = window.scrollY == 0 ? false : true;
+  }
 }
