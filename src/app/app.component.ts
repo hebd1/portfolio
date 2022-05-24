655f93c1efa80f8ddf5e895e80853809984a861b
hebd1 <elihebdon@gmail.com>
2022-05-24 00:27:03 -0600

    add progress animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import {
+  MatProgressBar,
+  ProgressAnimationEnd,
+} from '@angular/material/progress-bar';
+import {
+  delay,
+  distinctUntilChanged,
+  interval,
+  map,
+  Observable,
+  of,
+  startWith,
+  switchMap,
+  take,
+  takeWhile,
+  timer,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +25,19 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
   scrolled!: boolean;
+  $progressval: any;
+  @ViewChild(MatProgressBar) progressBar!: MatProgressBar;
+
+  ngAfterViewInit(): void {
+    this.$progressval = this.getProgressTimer(90);
+  }
+
+  public getProgressTimer(limit: number): Observable<any> {
+    return timer(0, 25).pipe(
+      map((progress) => (progress * 5 < 100 ? progress * 5 : 100)),
+      takeWhile((progress) => progress <= limit)
+    );
+  }
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
     this.scrolled = window.scrollY == 0 ? false : true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {} from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MatProgressBarModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
+  exports: [MatProgressBarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
