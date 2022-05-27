5cf1d5b341c2f8a42b25d7ace7ed88a9d53b7fa2
hebd1 <elihebdon@gmail.com>
2022-05-27 00:58:39 -0600

    get progress bar animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,19 +26,7 @@ export class AppComponent {
   $javascriptval: any;
   $goval: any;
 
-  ngAfterViewInit(): void {
-    this.$cssval = this.getProgressTimer(80);
-    this.$htmlval = this.getProgressTimer(90);
-    this.$javascriptval = this.getProgressTimer(75);
-    this.$goval = this.getProgressTimer(80);
-  }
-
-  public getProgressTimer(limit: number): any {
-    return timer(0, 25).pipe(
-      map((progress) => (progress * 5 < 100 ? progress * 5 : 100)),
-      takeWhile((progress) => progress <= limit)
-    );
-  }
+  ngAfterViewInit(): void {}
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
     this.scrolled = window.scrollY == 0 ? false : true;
--- src/app/directives/progress.directive.ts
+++ src/app/directives/progress.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { map, takeWhile, timer } from 'rxjs';
 
 @Directive({
   selector: '[appProgress]',
@@ -9,6 +10,13 @@ export class ProgressDirective implements OnInit {
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
-    this.el.nativeElement.style.width = this.appProgress + '%';
+    timer(0, 25)
+      .pipe(
+        map((progress) => (progress * 5 < 100 ? progress * 5 : 100)),
+        takeWhile((progress) => progress <= this.appProgress)
+      )
+      .subscribe((val) => {
+        this.el.nativeElement.style.width = val + '%';
+      });
   }
 }
