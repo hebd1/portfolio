1a3494cdb6cc02d51fbda520d5e8139cd1b166b9
hebd1 <elihebdon@gmail.com>
2022-05-27 19:36:50 -0600

    work on animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,40 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
 import {
-  delay,
-  distinctUntilChanged,
-  interval,
-  map,
-  Observable,
-  of,
-  startWith,
-  switchMap,
-  take,
-  takeWhile,
-  timer,
-} from 'rxjs';
+  fadeInOnEnterAnimation,
+  fadeOutOnLeaveAnimation,
+  slideInDownAnimation,
+  slideInDownOnEnterAnimation,
+  slideInLeftOnEnterAnimation,
+  slideInRightOnEnterAnimation,
+} from 'angular-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    fadeInOnEnterAnimation(),
+    slideInLeftOnEnterAnimation(),
+    fadeOutOnLeaveAnimation(),
+  ],
 })
 export class AppComponent {
+  @ViewChild('testDiv', { static: false })
+  private testDiv!: ElementRef<HTMLDivElement>;
+  isVisible!: boolean;
   title = 'portfolio';
   scrolled!: boolean;
-  $cssval: any;
-  $htmlval: any;
-  $javascriptval: any;
-  $goval: any;
 
   ngAfterViewInit(): void {}
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
     this.scrolled = window.scrollY == 0 ? false : true;
+    if (this.testDiv) {
+      const rect = this.testDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      this.isVisible = topShown && bottomShown;
+    }
   }
 }
